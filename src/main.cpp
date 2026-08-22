cc203e0d5de168f4a64674bcc67593cbc590ea5d
Qudix <17361645+Qudix@users.noreply.github.com>
2026-08-21 20:33:59 -0500

    feat: update lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
+SKSE_PLUGIN_PRELOAD(const SKSE::PreLoadInterface* a_skse)
+{
+	SKSE::Init(a_skse);
+
+	REX::INFO("Hello PreLoaded World!");
+
+	return true;
+}
+
 SKSE_PLUGIN_LOAD(const SKSE::LoadInterface* a_skse)
 {
 	SKSE::Init(a_skse);
 
-	REX::INFO("Hello World!");
+	REX::INFO("Hello Loaded World!");
 
 	return true;
 }
