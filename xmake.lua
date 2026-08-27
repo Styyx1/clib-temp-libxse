27a1fb751fc7d4f6da879bb52c93b90dcd453e71
Styyx <styyx228@gmail.com>
2026-08-28 00:19:40 +0200

    update for build system

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,30 @@
 -- include subprojects
-includes("lib/commonlibsse")
+includes("lib/commonlibsse", "extern/styyx-utils")
+
+local MOD_NAME = "PlaceHolder"
+local MOD_VERSION = "0.0.0"
+local MOD_DESC = "PlaceHolder"
 
 -- set project constants
-set_project("commonlibsse-template")
-set_version("0.0.0")
+set_project(MOD_NAME)
+set_version(MOD_VERSION)
 set_license("GPL-3.0")
 set_languages("c++23")
 set_warnings("allextra")
+
+--{{ADDITIONAL CONFIGS}}--
 
 -- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
 -- define targets
-target("commonlibsse-template")
+target(MOD_NAME)
+    add_deps("styyx-util")
     add_rules("commonlibsse.plugin", {
-        name = "commonlibsse-template",
-        author = "libxse",
-        description = "SKSE64 plugin template using CommonLibSSE"
+        name = MOD_NAME,
+        author = "styyx",
+        description = MOD_DESC
     })
 
     -- add src files
